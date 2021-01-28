1fc3b52392b37c0105ae50f134cdcc9b4a41a6af
gibranhackjust <78136331+gibranhackjust@users.noreply.github.com>
2021-01-28 15:12:58 +0800

    Add files via upload

---
+++ cyber.sh
@@ -0,0 +1,54 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay Gibran
+echo $green"SELAMAT DATANG DI TOOLS KAMI"
+echo $green"No WA : 0852-3850-6497"
+echo $green"Email : gibranfancy@gmail.com"
+echo $green"Blog  : gibranrima.com"
+echo $green"autor : M SYAESAR GIBRAN"
+echo
+echo
+echo $cyan"PILIH MENU HACKNYA BRO"
+echo
+echo $red"============================"
+echo $purple"[01].Hack SITUS/Website"
+echo $red"============================"
+echo $purple"[02].Hack Facebook Crack"
+echo $red"============================"
+echo $purple"[03].Hack Subscriber"
+echo $red"============================"
+echo $purple"[04].Kirim Virus"
+echo $red"============================"
+echo $purple"[05].Hack Diamond FF"
+echo $red"============================"
+echo $purple"[06].Hack Instagram"
+echo $red"============================"
+echo $purple"[07].Hack UC pubg"
+echo $red"============================"
+echo $purple"[08].Hack diamond MLBB"
+echo $red"============================"
+echo $purple"[09].Red-HAWK"
+echo $red"============================"
+echo $purple"[10].Hack Akun Game"
+echo $red"============================"
+echo $purple"[11].Sadap WhatsApp"
+echo $red"============================"
+echo $purple"[12].Dark FB diamond"
+echo $red"============================"
+echo $purple"[13].Root Handphone Semua Merk"
+echo $red"============================"
+echo $purple"[14].Hack Kuota ALL kartu"
+echo $red"============================"
+echo $purple"[15].Hack Pulsa ALL"
+echo $red"============================"
+echo $purple"[16].Keluar"
+echo $red"============================"
+
